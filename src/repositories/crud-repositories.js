639756bdef4f9f79e11a9a1dcf9c1705f1b2a5e7
Sourav <ksourav662@gmail.com>
2026-01-11 00:33:48 +0530

    feat(booking): add booking MVC boilerplate

---
+++ src/repositories/crud-repositories.js
@@ -0,0 +1,48 @@
+const { StatusCodes } = require('http-status-codes');
+const AppError = require('../utils/errors/app-error');
+
+class CrudRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    const response = await this.model.create(data);
+    return response;
+  }
+
+  async get(id) {
+    const response = await this.model.findByPk(id);
+    if (!response) {
+      throw new AppError('Data not found from the given id', StatusCodes.NOT_FOUND);
+    }
+    return response;
+  }
+
+  async getAll() {
+    const response = await this.model.findAll();
+    return response;
+  }
+
+  async update(id, data) {
+    const response = await this.model.update(data, {
+      where: { id },
+    });
+    if (!response[0]) {
+      throw new AppError('Data not found from the given id', StatusCodes.NOT_FOUND);
+    }
+    return response;
+  }
+
+  async delete(id) {
+    const response = await this.model.destroy({
+      where: { id },
+    });
+    if (!response) {
+      throw new AppError('Data not found from the given id', StatusCodes.NOT_FOUND);
+    }
+    return response;
+  }
+}
+
+module.exports = CrudRepository;
