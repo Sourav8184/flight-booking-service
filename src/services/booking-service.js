09baed970180e1df687031ecf24da389ea1e66dc
Sourav <ksourav662@gmail.com>
2026-01-15 00:39:49 +0530

    feat(booking): Added cancel booking logic

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -71,11 +71,7 @@ async function makePayment(data) {
     const timeDiff = (currentTime - bookingTime) / (1000 * 60); // time difference in minutes
 
     if (timeDiff > 20) {
-      await bookingRepository.updateBookingDetails(
-        data.bookingId,
-        { status: ENUMS.BOOKING_STATUS.CANCELLED },
-        transaction,
-      );
+      await cancelBooking(data.bookingId);
       throw new AppError('Booking Expired', StatusCodes.BAD_REQUEST);
     }
 
@@ -102,6 +98,44 @@ async function makePayment(data) {
       throw error;
     }
     throw new AppError('Payment Failed', StatusCodes.BAD_REQUEST);
+  }
+}
+
+async function cancelBooking(bookingId) {
+  const transaction = await db.sequelize.transaction();
+  try {
+    const bookingDetail = await bookingRepository.getBookingDetails({ bookingId }, transaction);
+
+    // if already cancelled, return
+    if (bookingDetail.status === ENUMS.BOOKING_STATUS.CANCELLED) {
+      await transaction.commit();
+      throw new AppError('Booking is already cancelled', StatusCodes.BAD_REQUEST);
+    }
+
+    // increment the seats in flight service
+    await axios.patch(
+      `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${bookingDetail.flightId}/seats`,
+      {
+        seats: bookingDetail.totalSeats,
+        dec: false,
+      },
+    );
+
+    // update the booking status to cancelled
+    const updatedBooking = await bookingRepository.updateBookingDetails(
+      bookingId,
+      { status: ENUMS.BOOKING_STATUS.CANCELLED },
+      transaction,
+    );
+
+    await transaction.commit();
+    return updatedBooking;
+  } catch (error) {
+    await transaction.rollback();
+    if (error instanceof AppError) {
+      throw error;
+    }
+    throw new AppError('Cannot cancel booking', StatusCodes.BAD_REQUEST);
   }
 }
 
